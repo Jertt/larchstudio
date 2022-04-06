5b9348dc309107c8462406dfcef9d8f4bcf66653
Jertt <99461163+Jertt@users.noreply.github.com>
2022-04-06 20:30:42 +0200

    larch project`
    This is a website for an architectural designer called Lir Berisha.

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'body': '#f0f0f0',
+        'selected-text': '#000000',
+
+      },
+      fontFamily: {
+        rota: "'Rota', serif",
+        rotaeb: "'Rotaeb', serif",
+        rotab: "'Rotab', serif",
+        rotareg: "'Rotareg', serif",
+      },
+    },
+  },
+  plugins: [],
+}
